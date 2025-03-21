21e638fd304bb8fda938b8eb7290e3d0d0f594d4
Sama Manikanta Reddy <97392940+sama-manikanta-reddy@users.noreply.github.com>
2025-03-21 11:07:05 +0530

    Update run-Framma-C.sh - added {} to variables

--- static-analysis/run-Framma-C.sh
+++ static-analysis/run-Framma-C.sh
@@ -1,4 +1,3 @@
-# Add script here
 #!/bin/bash
 
 # Ensure the file is provided as an argument
@@ -17,22 +16,22 @@ FILE_PATH=$(realpath "$FILE_NAME") || { echo "Error: Unable to resolve file path
 FILE_NAME=$(basename "$FILE_NAME" .c)
 
 # Create the directory to store output files
-SAVE_DIR=${INSTALL_PATH}/C-Tools/StaticAnalysis/Results/${FILE_NAME}/framac
-mkdir -p ${SAVE_DIR} || { echo "Error: Failed to create directory ${FILE_NAME}/framac"; exit 1; }
+SAVE_DIR=${INSTALL_PATH}/C-Tools/StaticAnalysis/Results/${FILE_NAME}/frammac
+mkdir -p ${SAVE_DIR} || { echo "Error: Failed to create directory ${SAVE_DIR}"; exit 1; }
 
 # Run Frama-C analysis
 frama-c -eva -eva-no-remove-redundant-alarms \
     -eva-all-rounding-modes-constants \
     -eva-join-results -eva-report-red-statuses \
-    "${SAVE_DIR}/$FILE_NAME-red" "$FILE_PATH" &> "${SAVE_DIR}/$FILE_NAME-report.txt" || { echo "Error: Frama-C analysis failed"; exit 1; }
+    "${SAVE_DIR}/${FILE_NAME}-red" "$FILE_PATH" &> "${SAVE_DIR}/${FILE_NAME}-report.txt" || { echo "Error: Frama-C analysis failed"; exit 1; }
 
 # Process the output of Frama-C
-sed '/division_by_zero/!d' "${SAVE_DIR}/$FILE_NAME-red" | 
+sed '/division_by_zero/!d' "${SAVE_DIR}/${FILE_NAME}-red" | 
     cut -f1,2,4-9 --complement |
-    sort -u > "${SAVE_DIR}/$FILE_NAME-lines.txt" || { echo "Error: Sed/cut/sort processing failed"; exit 1; }
+    sort -u > "${SAVE_DIR}/${FILE_NAME}-lines.txt" || { echo "Error: Sed/cut/sort processing failed"; exit 1; }
 
 # Modify the lines and save to a new file
-sed "s/^/$FILE_NAME-/; s/$/.c/" "${SAVE_DIR}/$FILE_NAME-lines.txt" > "${SAVE_DIR}/$FILE_NAME-hard.txt" || { echo "Error: Sed transformation failed"; exit 1; }
+sed "s/^/$FILE_NAME-/; s/$/.c/" "${SAVE_DIR}/${FILE_NAME}-lines.txt" > "${SAVE_DIR}/${FILE_NAME}-hard.txt" || { echo "Error: Sed transformation failed"; exit 1; }
 
 # Copy the original file to the directory
 cp "$FILE_PATH" "${SAVE_DIR}" || { echo "Error: Failed to copy $FILE_PATH to ${SAVE_DIR}/"; exit 1; }
